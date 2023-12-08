89b16ab2d0219559c5f51303056293046d964290
stephenmdevine <stephenmdevine@gmail.com>
2023-12-07 18:48:34 -0600

    Task 1 complete

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -119,8 +119,6 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
-//        System.out.println("printJobs is not implemented yet");
         if (someJobs.size() == 0){
             System.out.println("No Results");
         }   else {
